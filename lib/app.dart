94c7eef641c2e4128ecde2738e025f371fe70800
HongshuW <hongshu.wang.uni@gmail.com>
2021-05-19 23:51:34 +0800

    Installed cloud_firestore: ^2.2.0

--- lib/app.dart
+++ lib/app.dart
@@ -5,9 +5,12 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Login App',
-      theme: ThemeData(accentColor: Colors.orange, primarySwatch: Colors.blue),
-      home: LoginScreen(),
-    );
+        title: 'Login App',
+        theme: ThemeData(accentColor: Colors.orange, primarySwatch: Colors.blue),
+        home: LoginScreen(),
+      );
   }
+}
+
+class ChangeNotifierProvider {
 }
