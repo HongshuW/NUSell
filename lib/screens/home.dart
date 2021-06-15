162e987c8d91d35c722d10a347862f495acc3e19
Liu <e0559386@u.nus.edu>
2021-06-15 23:06:22 +0800

    added shopping cart to home

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -25,188 +25,217 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  // firebase fields
   final FirebaseAuth auth = FirebaseAuth.instance;
   final FirebaseFirestore db = FirebaseFirestore.instance;
   final FirebaseStorage storage = FirebaseStorage.instance;
 
+  // filters fields
   int type;
+
+  // back to top fields
+  ScrollController scrollController = ScrollController();
 
   @override
   Widget build(BuildContext context) {
+    // Providers
     final userinfoProvider =
         Provider.of<userInfoProvider>(context, listen: false);
-
     final posts = Provider.of<postsProvider>(context);
-
     final selected = Provider.of<filtersProvider>(context);
 
     return ChangeNotifierProvider(
       create: (context) => postsProvider(),
-      child: Scaffold(
-        resizeToAvoidBottomInset: false,
+      child: GestureDetector(
+        onTap: () {
+          FocusScope.of(context).requestFocus(new FocusNode());
+        },
+        child: Scaffold(
+          resizeToAvoidBottomInset: false,
 
-        // search bar
-        appBar: AppBar(
-          automaticallyImplyLeading: false,
-          backgroundColor: Color.fromRGBO(242, 195, 71, 1),
-          elevation: 0,
-          title: Container(
-            margin: EdgeInsets.only(left: 40, right: 40),
-            child: TextField(
-              onTap: () {
-                if (auth.currentUser == null) {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => LoginScreen()));
-                } else {
-                  Navigator.of(context).push(
-                      MaterialPageRoute(builder: (context) => SearchBar()));
-                }
-              },
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(color: Colors.black),
-                  borderRadius: BorderRadius.circular(20),
+          // search bar
+          appBar: AppBar(
+            automaticallyImplyLeading: false,
+            backgroundColor: Color.fromRGBO(242, 195, 71, 1),
+            elevation: 0,
+            title: Container(
+              margin: EdgeInsets.only(left: 40, right: 40),
+              child: TextField(
+                onTap: () {
+                  if (auth.currentUser == null) {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => LoginScreen()));
+                  } else {
+                    Navigator.of(context).push(
+                        MaterialPageRoute(builder: (context) => SearchBar()));
+                  }
+                },
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(color: Colors.black),
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                  hintText: "Search",
+                  isDense: true,
+                  contentPadding: EdgeInsets.only(left: 10, top: 5, bottom: 5),
+                  fillColor: Color.fromRGBO(249, 248, 253, 1),
+                  filled: true,
                 ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(20),
-                ),
-                hintText: "Search",
-                isDense: true,
-                contentPadding: EdgeInsets.only(left: 10, top: 5, bottom: 5),
-                fillColor: Color.fromRGBO(249, 248, 253, 1),
-                filled: true,
               ),
             ),
           ),
-        ),
 
-        body: Container(
-          decoration: BoxDecoration(
-            color: Color.fromRGBO(249, 248, 253, 1),
-          ),
-          child: ListView(
-            children: [
-              Container(
-                padding: EdgeInsets.only(left: 30, right: 30),
-                decoration: BoxDecoration(
-                  gradient: LinearGradient(
-                    begin: Alignment.topCenter,
-                    end: Alignment.bottomCenter,
-                    colors: [
-                      Color.fromRGBO(242, 195, 71, 1),
-                      Color.fromRGBO(252, 228, 70, 1),
+          body: Container(
+            decoration: BoxDecoration(
+              color: Color.fromRGBO(249, 248, 253, 1),
+            ),
+            child: ListView(
+              controller: scrollController,
+              children: [
+                Container(
+                  padding: EdgeInsets.only(left: 30, right: 30),
+                  decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                      begin: Alignment.topCenter,
+                      end: Alignment.bottomCenter,
+                      colors: [
+                        Color.fromRGBO(242, 195, 71, 1),
+                        Color.fromRGBO(252, 228, 70, 1),
+                      ],
+                    ),
+                    borderRadius: BorderRadius.only(
+                      bottomLeft: Radius.circular(30),
+                      bottomRight: Radius.circular(30),
+                    ),
+                  ),
+
+                  // filters
+                  child: Column(
+                    children: [
+                      IconButton(
+                          icon: Icon(Icons.shopping_cart),
+                          onPressed: () {
+                            Navigator.of(context).push(MaterialPageRoute(
+                                builder: (context) => MyShoppingCartsScreen(
+                                      userId: AuthService().getCurrentUID(),
+                                    )));
+                          }),
+                      CustomRadioButton(
+                        elevation: 0,
+                        padding: 0,
+                        enableShape: true,
+                        enableButtonWrap: true,
+                        width: 120,
+                        wrapAlignment: WrapAlignment.center,
+                        buttonLables: [
+                          'Time Posted',
+                          'Category',
+                          'Location',
+                          'Price',
+                          'Clear Filters'
+                        ],
+                        buttonValues: [1, 2, 3, 4, 5],
+                        buttonTextStyle: ButtonTextStyle(
+                          selectedColor: Color.fromRGBO(190, 140, 90, 1),
+                          unSelectedColor: Colors.white,
+                          textStyle: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w300,
+                          ),
+                        ),
+                        radioButtonValue: (value) => {
+                          setState(() {
+                            this.type = value;
+                          })
+                        },
+                        unSelectedColor: Colors.transparent,
+                        selectedColor: Color.fromRGBO(250, 190, 90, 0.5),
+                        unSelectedBorderColor: Colors.white,
+                        selectedBorderColor: Color.fromRGBO(190, 140, 90, 1),
+                      ),
+                      Filter(type: this.type),
+                      selectedFilters(),
                     ],
                   ),
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: Radius.circular(30),
-                    bottomRight: Radius.circular(30),
-                  ),
                 ),
 
-                // filters
-                child: Column(
-                  children: [
-                    IconButton(
-                        icon: Icon(Icons.shopping_cart),
-                        onPressed: () {
-                          Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => MyShoppingCartsScreen(
-                                    userId: AuthService().getCurrentUID(),
-                                  )));
-                        }),
-                    CustomRadioButton(
-                      elevation: 0,
-                      padding: 0,
-                      enableShape: true,
-                      enableButtonWrap: true,
-                      width: 120,
-                      wrapAlignment: WrapAlignment.center,
-                      buttonLables: [
-                        'Time Posted',
-                        'Category',
-                        'Location',
-                        'Price',
-                        'Clear Filters'
-                      ],
-                      buttonValues: [1, 2, 3, 4, 5],
-                      buttonTextStyle: ButtonTextStyle(
-                        selectedColor: Color.fromRGBO(190, 140, 90, 1),
-                        unSelectedColor: Colors.white,
-                        textStyle: TextStyle(
-                          fontSize: 15,
-                          fontWeight: FontWeight.w300,
-                        ),
-                      ),
-                      radioButtonValue: (value) => {
-                        setState(() {
-                          this.type = value;
-                        })
-                      },
-                      unSelectedColor: Colors.transparent,
-                      selectedColor: Color.fromRGBO(250, 190, 90, 0.5),
-                      unSelectedBorderColor: Colors.white,
-                      selectedBorderColor: Color.fromRGBO(190, 140, 90, 1),
-                    ),
-                    Filter(type: this.type),
-                    selectedFilters(),
-                  ],
-                ),
-              ),
-
-              // display posts
-              allPosts(),
-            ],
+                // display posts
+                allPosts(),
+              ],
+            ),
           ),
-        ),
 
-        //a collection of three floating action buttons, on pressed will
-        //turn to another page
-        floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-        floatingActionButton: Container(
-          padding: const EdgeInsets.only(left: 16.0, right: 16.0),
-          height: 80,
-          color: Colors.white,
-          child: Row(
-            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            children: <Widget>[
-              FloatingActionButton(
-                heroTag: "home",
-                onPressed: () {},
-                child: Icon(Icons.house, color: Colors.white),
-                backgroundColor: Color.fromRGBO(247, 215, 140, 1),
-              ),
-              FloatingActionButton(
-                heroTag: "post",
-                onPressed: () {
-                  if (auth.currentUser == null) {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => LoginScreen()));
-                  } else {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => PostScreen()));
-                  }
-                },
-                child: Icon(Icons.add, color: Colors.white),
-                backgroundColor: Color.fromRGBO(242, 195, 71, 1),
-              ),
-              FloatingActionButton(
-                heroTag: "profile",
-                onPressed: () {
-                  if (auth.currentUser == null) {
-                    Navigator.of(context).push(
-                        MaterialPageRoute(builder: (context) => LoginScreen()));
-                  } else {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => ProfileScreen()));
-                  }
-                },
-                child: Icon(
-                  Icons.person,
-                  color: Colors.white,
+          // a collection of three floating action buttons, on pressed will
+          // turn to another page
+          floatingActionButtonLocation:
+              FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: Container(
+            padding: const EdgeInsets.only(left: 16.0, right: 16.0),
+            height: 80,
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: <Widget>[
+                FloatingActionButton(
+                  heroTag: "home",
+                  onPressed: () {
+                    scrollController.animateTo(
+                        scrollController.position.minScrollExtent,
+                        duration: Duration(microseconds: 500),
+                        curve: Curves.fastOutSlowIn);
+                  },
+                  child: Icon(Icons.house, color: Colors.white),
+                  backgroundColor: Color.fromRGBO(247, 215, 140, 1),
                 ),
-                backgroundColor: Color.fromRGBO(242, 195, 71, 1),
-              )
-            ],
+                FloatingActionButton(
+                  heroTag: "post",
+                  onPressed: () {
+                    if (auth.currentUser == null) {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => LoginScreen()));
+                    } else {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => PostScreen()));
+                    }
+                  },
+                  child: Icon(Icons.add, color: Colors.white),
+                  backgroundColor: Color.fromRGBO(242, 195, 71, 1),
+                ),
+                FloatingActionButton(
+                  heroTag: "profile",
+                  onPressed: () {
+                    if (auth.currentUser == null) {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => LoginScreen()));
+                    } else {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => ProfileScreen()));
+                    }
+                  },
+                  child: Icon(
+                    Icons.person,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Color.fromRGBO(242, 195, 71, 1),
+                ),
+                FloatingActionButton(
+                  heroTag: "top",
+                  onPressed: () {
+                    scrollController.animateTo(
+                        scrollController.position.minScrollExtent,
+                        duration: Duration(microseconds: 500),
+                        curve: Curves.fastOutSlowIn);
+                  },
+                  child: Icon(
+                    Icons.arrow_upward,
+                    color: Colors.white,
+                  ),
+                  backgroundColor: Colors.blue,
+                )
+              ],
+            ),
           ),
         ),
       ),
