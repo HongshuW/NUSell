bd4db684e15be569c1ac003cdfe69459248138f7
Liu <e0559386@u.nus.edu>
2021-06-16 17:57:27 +0800

    changed some minor ui design

--- lib/screens/allPosts.dart
+++ lib/screens/allPosts.dart
@@ -54,7 +54,6 @@ class _allPostsState extends State<allPosts> {
           .orderBy("time", descending: true)
           .limit(numPerPage)
           .get();
-      print('lastdoc is null');
     } else {
       querySnapshot = await FirebaseFirestore.instance
           .collection("posts")
@@ -62,7 +61,6 @@ class _allPostsState extends State<allPosts> {
           .startAfterDocument(lastDoc)
           .limit(numPerPage)
           .get();
-      print('loading');
     }
     int len = querySnapshot.docs.length;
     if (len < numPerPage) {
