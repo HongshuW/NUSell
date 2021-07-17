7ac42f21dd28d5f9bcccfcc7bc86954d486a7e03
HongshuW <hongshu.wang.uni@gmail.com>
2021-07-17 18:08:02 +0800

    Implemented reply feature

--- lib/models/comment.dart
+++ lib/models/comment.dart
@@ -1,13 +1,11 @@
 class Comment {
   String content;
   String user;
+  String mention;
 
-  Comment({this.content, this.user});
+  Comment({this.content, this.user, this.mention});
 
   Map<String, dynamic> toMap() {
-    return {'user': this.user, 'message': this.content, 'replies': []};
+    return {'user': user, 'message': content, 'mention': mention};
   }
-}
-
-class Reply extends Comment {
 }
