2cff02c38e8b5be4e0d1fbeb1becb4b32caf1892
HongshuW <hongshu.wang.uni@gmail.com>
2021-09-19 16:28:02 +0800

    TermsAndConditionsScreen class: implement build

--- lib/screens/termsAndConditions.dart
+++ lib/screens/termsAndConditions.dart
@@ -1,13 +1,68 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class TermsAndConditionsScreen extends StatelessWidget {
+  var db = FirebaseFirestore.instance;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Terms and Services"),
       ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.all(30),
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/images/background.png"),
+              fit: BoxFit.fill,
+              alignment: Alignment.bottomRight),
+        ),
+        child: StreamBuilder(
+          stream: db.collection("about").doc("Policies").snapshots(),
+          builder: (context, snapshot) {
+            if (!snapshot.hasData) {
+              return Center(child: CircularProgressIndicator());
+            }
+            var policies = snapshot.data.data();
+            return ListView(
+              children: [
+                // privacy policy
+                Container(
+                  margin: EdgeInsets.only(top: 20, bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Privacy Policy",
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(policies["privacyPolicy"]),
+
+                // terms and conditions
+                Container(
+                  margin: EdgeInsets.only(top: 50, bottom: 10),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Terms and Conditions",
+                        style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+                      ),
+                    ],
+                  ),
+                ),
+                Text(policies["termsAndConditions"]),
+              ],
+            );
+          },
+        )
+      )
     );
   }
 }
